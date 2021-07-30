037d0fe3589d3b11c60e8a37155cd23e8c98a047
luipalencia <luipalencia@gmail.com>
2021-07-30 12:33:13 -0400

    add cleanup and useFetch function as custom hook

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>My own blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/creat" style={{color: 'white', backgroundColor: 'red'}}>New blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{color: 'white', backgroundColor: 'red'}}>New blog</Link>
 
             </div>
         </nav>
