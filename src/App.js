bac52459206e363bd763405ac8177b4efcf7f4e4
luipalencia <luipalencia@gmail.com>
2021-08-05 18:25:56 -0400

    Add function to delete blog and error page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound.js'
 
 function App() {
 
@@ -17,6 +19,12 @@ function App() {
             <Route path="/create">
               <Create />
             </Route>
+            <Route path="/blogs/:id">
+             <BlogDetails />
+            </Route> 
+            <Route path="*">
+             <NotFound />
+            </Route> 
           </Switch>
         </div>
       </div>
--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+
 const Create = () => {
+    const [ title, setTitle] = useState('');
+    const [ author, setAuthor] = useState('');
+    const [ body, setBody] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory(); 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        console.log(blog)
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+            history.push('/');
+        })
+  
+    }
+    
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label> Blog title :</label>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required/>
+                 <label> Blog body :</label>
+            <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+            <label> Blog author : </label>
+            <select value={author} onChange={(e) => setAuthor(e.target.value)}> 
+                <option value="angel">Angel</option>
+                <option value="luimar">Luimar</option>
+            </select>
+            { !isPending &&  <button>Add blog</button> }
+            { isPending &&  <button disabled>Adding blog...</button> }
+            </form>
         </div>
      );
 }
